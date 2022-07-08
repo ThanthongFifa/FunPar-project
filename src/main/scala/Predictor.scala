d9f5eee705edd700597654e93aa34351e2e6626b
ThanthongFifa <Fifa0962@gmail.com>
2022-07-08 11:23:44 +0700

    implement getVolatility

--- src/main/scala/Predictor.scala
+++ src/main/scala/Predictor.scala
@@ -15,6 +15,20 @@ object Predictor extends App {
   // The numbers of openingPrices are in the wrong order for some reason.
   // Currently can't figure out why but the parser seems to randomly pick the order of opening prices to put in.
   // https://index.scala-lang.org/playframework/play-json
-  println(getOpeningPrices("IBM"))
+  val prices = getOpeningPrices("IBM")
+  println(prices)
+
+  def getVolatility(jsPrices: scala.collection.Seq[JsValue]): Double = {
+    val prices = jsPrices.map(i => i.as[String].toFloat)
+    val n = prices.length
+    val mean = prices.sum/n
+
+    val sd = prices.map(i => math.pow((i - mean),2)).sum/n
+    math.sqrt(sd)/1000
+  }
+
+  val volatility = getVolatility(prices)
+  println(volatility)
+
 
 }
