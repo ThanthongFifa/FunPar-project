e8026a8f00cb6cb6422a6514de352297c1ca9821
Green-Tea <sittingducks.lorenzo@gmail.com>
2022-07-11 16:46:19 +0700

    changed getOpeningPrices to return Seq[Float]

--- src/main/scala/Predictor.scala
+++ src/main/scala/Predictor.scala
@@ -5,21 +5,16 @@ object Predictor extends App {
   // Returns a list of opening prices of each day over the last 100 days
   // API key is limited to 500 requests per day, 5 per minute
   // https://www.alphavantage.co/documentation/
-  def getOpeningPrices(stock: String): scala.collection.Seq[JsValue] = {
+  def getOpeningPrices(stock: String): scala.collection.Seq[Float] = {
     val r = requests.get(s"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=$stock&apikey=E007RI6Q8GHF36VA").data
     val json: JsValue = Json.parse(s"""$r""")
     val openingPrices = json \\ "1. open"
-    openingPrices
+    openingPrices.map(i => i.as[String].toFloat)
   }
-
-  // The numbers of openingPrices are in the wrong order for some reason.
-  // Currently can't figure out why but the parser seems to randomly pick the order of opening prices to put in.
   // https://index.scala-lang.org/playframework/play-json
-  val prices = getOpeningPrices("IBM")
-  println(prices)
 
-  def getVolatility(jsPrices: scala.collection.Seq[JsValue]): Double = {
-    val prices = jsPrices.map(i => i.as[String].toFloat)
+
+  def getVolatility(prices: scala.collection.Seq[Float]): Double = {
     val n = prices.length
     val mean = prices.sum/n
 
@@ -27,7 +22,9 @@ object Predictor extends App {
     math.sqrt(sd)/1000
   }
 
-  val volatility = getVolatility(prices)
+  val openingPricesIBM = getOpeningPrices("IBM")
+  val volatility = getVolatility(openingPricesIBM)
+  println(openingPricesIBM)
   println(volatility)
 
 
