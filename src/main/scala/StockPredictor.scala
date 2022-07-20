2502df1f5e46eb231cc07f2ae232cb8293734e00
ThanthongFifa <Fifa0962@gmail.com>
2022-07-20 19:29:40 +0700

    Implement nextPrice
    use GBM to get next day price

--- src/main/scala/StockPredictor.scala
+++ src/main/scala/StockPredictor.scala
@@ -1,3 +1,5 @@
+import scala.util.Random
+
 /**
  * The class that will take value below to simulate the movement of a stock for the next <period> days.
  * @param name: this discribe the name of the stock.
@@ -8,8 +10,20 @@
  */
 class StockPredictor(val name: String, currentPrice: Double, expectedReturn: Double, period: Double, votality: Double) {
 
+  /**
+   * override toString so it will be easier to read
+   * @return
+   */
   override def toString: String = {
     s"Name: $name \nCurrent Price: $currentPrice \nExpected Return: $expectedReturn \nTime Period: $period \nVotality: $votality"
+  }
+
+  /**
+   * use GBM to get next day price
+   * @return currentPrice + predicted change in price
+   */
+  def nextPrice(): Double ={
+    return currentPrice + (currentPrice * (expectedReturn * period + votality * math.sqrt(period) * Random.nextGaussian()))
   }
 
 }
