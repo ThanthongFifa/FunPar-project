43cf5058ba218a21462471c0c9d23bfe0fe956b3
ThanthongFifa <Fifa0962@gmail.com>
2022-07-21 09:04:40 +0700

    make StockPredictor() take less parameters

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,20 +1,8 @@
-import Retriever.{getExpectedReturn, getOpeningPrices, getVolatility}
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
-import scala.util.{Failure, Random, Success}
 
 object Main {
   def main(args: Array[String]): Unit = {
 
-    // setting up necessary info
-    val openingPricesIBM = getOpeningPrices("IBM")
-    val volatility = getVolatility(openingPricesIBM)
-    val returnRate = getExpectedReturn(openingPricesIBM)
-
-    // create StockPredictor for IBM
-    var IBM = new StockPredictor("IBM", 130.88, returnRate, 252, volatility) //252 is number of working days
+    val IBM = new StockPredictor("IBM", 130.88, 252) //252 is number of working days
 
     println(IBM.toString)
     println(IBM.nextPrice())
--- src/main/scala/StockPredictor.scala
+++ src/main/scala/StockPredictor.scala
@@ -1,3 +1,5 @@
+import Retriever.{getExpectedReturn, getOpeningPrices, getVolatility}
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import scala.util.{Failure, Random, Success}
@@ -7,12 +9,13 @@ import scala.concurrent.{Await, Future, Promise}
  * The class that will take value below to simulate the movement of a stock for the next <period> days.
  * @param name           : this discribe the name of the stock.
  * @param currentPrice   : starting price (today price).
- * @param expectedReturn : expected ravenue of this stock.
  * @param period         : the length of time in the future in days.
- * @param votality       : the votality of the stock.
  */
-class StockPredictor(val name: String, currentPrice: Double, expectedReturn: Double, period: Double, votality: Double) {
+class StockPredictor(val name: String, currentPrice: Double, period: Double) {
   val dt = 1 / period
+  val openingPrices = getOpeningPrices(name)
+  val votality = getVolatility(openingPrices)
+  val expectedReturn = getExpectedReturn(openingPrices)
 
   /**
    * override toString so it will be easier to read
@@ -27,11 +30,11 @@ class StockPredictor(val name: String, currentPrice: Double, expectedReturn: Dou
    * @return currentPrice + predicted change in price
    */
   def nextPrice(): Double = {
-    return currentPrice + (currentPrice * ((expectedReturn * dt) + (votality * math.sqrt(dt) * Random.nextGaussian())))
+    currentPrice + (currentPrice * ((expectedReturn * dt) + (votality * math.sqrt(dt) * Random.nextGaussian())))
   }
 
   def nextPrice(cp: Double): Double = {
-    return cp + (cp * ((expectedReturn * dt) + (votality * math.sqrt(dt) * Random.nextGaussian())))
+    cp + (cp * ((expectedReturn * dt) + (votality * math.sqrt(dt) * Random.nextGaussian())))
   }
 
   /**
