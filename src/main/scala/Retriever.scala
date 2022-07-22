b9a3107c0fbbabe93ccf99f200037cf92a9ae679
ThanthongFifa <Fifa0962@gmail.com>
2022-07-22 13:14:58 +0700

    fix getOpeningPrices returning 1price/15mins to 1price/ day
    this give use 21 days worth of opening price.

--- src/main/scala/Retriever.scala
+++ src/main/scala/Retriever.scala
@@ -16,20 +16,26 @@ object Retriever extends App {
   }
 
   // get opening prices at 15 minute intervals within the past 30 days
-  // FIXME: Lorenzo, plz make this function return 1entry/day. currently return 1/15 mins
-  // - if possible make this concurrent
-  // - if possible, get other stock info like P/E, dividend, ect.
-  // - and maybe calculate avg trade volume/day.
-  // - Lastly, put those info in the report in StockPredictor
+  // FIXME:
+  //  - Lorenzo, plz make this function return 1entry/day. currently return 1/15 mins *****Fixed
+  //  - if possible make this concurrent
+  //  - if possible, get other stock info like P/E, dividend, ect.
+  //  - and maybe calculate avg trade volume/day.
+  //  - Lastly, put those info in the report in StockPredictor
   def getOpeningPrices(stock: String): List[Float] = {
     val csv = downloadCsv(s"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=$stock&interval=15min&slice=year1month1&apikey=E007RI6Q8GHF36VA",
       s"$stock.csv")
 
     val bufferedSource = Source.fromFile(s"$stock.csv")
     var openingPricesMut = new ListBuffer[Float]()
+    var date = ""
     for (line <- bufferedSource.getLines.drop(1)) {
       val cols = line.split(",").map(_.trim)
-      openingPricesMut += cols(1).toFloat
+      val thisDate = cols(0).split(" ")(0)
+      if (!thisDate.equals(date)) {
+        openingPricesMut += cols(1).toFloat
+        date = thisDate
+      }
     }
     bufferedSource.close
     openingPricesMut.toList.reverse
