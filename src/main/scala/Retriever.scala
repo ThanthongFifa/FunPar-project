89bafa88917f3bc7f30f8d604d9ff40f1c46d2df
Green-Tea <sittingducks.lorenzo@gmail.com>
2022-07-23 18:05:59 +0700

    Calculate opening prices and total trade volume in functional style

--- src/main/scala/Retriever.scala
+++ src/main/scala/Retriever.scala
@@ -5,6 +5,7 @@ import java.net.URL
 import java.io.File
 import scala.language.postfixOps
 
+
 object Retriever extends App {
   // API key is limited to 500 requests per day, 5 per minute
   // https://www.alphavantage.co/documentation/
@@ -15,31 +16,27 @@ object Retriever extends App {
     new URL(url) #> new File(filename) !!
   }
 
-  // get opening prices at 15 minute intervals within the past 30 days
   // FIXME:
-  //  - Lorenzo, plz make this function return 1entry/day. currently return 1/15 mins *****Fixed
   //  - if possible make this concurrent
   //  - if possible, get other stock info like P/E, dividend, ect.
-  //  - and maybe calculate avg trade volume/day. *****DONE but i switch to total trade volume
   //  - Lastly, put those info in the report in StockPredictor
+
   def getOpeningPrices(stock: String): (List[Float],Int) = {
     if (!new java.io.File(s"$stock.csv").isFile){
-      println("Downloading CSV")
+      println("No file found, downloading new csv file...")
       val csv = downloadCsv(s"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=$stock&apikey=E007RI6Q8GHF36VA&datatype=csv",
         s"$stock.csv")
+      println("Download completed.")
     }
 
     val bufferedSource = Source.fromFile(s"$stock.csv")
-    var openingPricesMut = new ListBuffer[Float]()
-    var date = ""
-    var totalTradeVolume = 0
-    for (line <- bufferedSource.getLines.drop(1)) {
-      val cols = line.split(",").map(_.trim)
-      totalTradeVolume =  totalTradeVolume + cols.last.toInt
-      openingPricesMut += cols(1).toFloat
-    }
-    bufferedSource.close
-    (openingPricesMut.toList.reverse,totalTradeVolume)
+    val data = bufferedSource.getLines.drop(1).map(_.split(",").map(_.trim)).toList
+    val openingPrices: List[Float] = data.map(_(1).toFloat)
+    val tradeVolume: List[Int] = data.map(_(5).toInt)
+    val totalTradeVolume = tradeVolume.sum
+    bufferedSource.close()
+
+    (openingPrices.reverse, totalTradeVolume)
   }
 
   def getVolatility(prices: scala.collection.Seq[Float], n: Double): Double = {
